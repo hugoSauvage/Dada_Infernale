3cb52ae580f468cf2a151462615e3ddd7ee497ba
Macquet Quentin <qunetinmacquet@gmail.com>
2023-11-21 09:14:57 +0100

    ADD: Horse
    début : methodes et init

---
+++ Horse.py
@@ -0,0 +1,27 @@
+class Horse(object):
+    
+    def __init__(self, couleur, nom, position = 0) -> None:
+        self.couleur = couleur
+        self.position = position
+        self.nom = nom
+        
+    def seDeplacer(self, nb_case):
+        pass
+    
+    def get_position(self):
+        return self.position
+
+    def est_en_vie():
+        pass
+
+    def sauter():
+        pass     
+
+    def sortir_maison():
+        pass
+    
+    def entree_maison():
+        pass
+    
+    def chemin_paradis():
+        pass
