79dba03bd9004b107c4cd5fc5600bffac5a49c9f
Enzo Danel <Enzodanel59@gmail.com>
2023-11-21 09:27:56 +0100

    ADD: init screen
    Création de la premiere fenetre qui se lance , ainsi que le fichier main.py , screen.py et game.py. Avec pour class respective Screen et Game

---
+++ MAP/game.py
@@ -0,0 +1,12 @@
+from screen import Screen
+import pygame
+class Game():
+    def __init__(self) -> None:
+        self.running = True
+        self.screen = Screen()
+
+    def run(self):
+        while self.running:
+            pass
+
+
