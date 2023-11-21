76c44dfebbde43fa3efa3e8b2e5ccd8dd4eac080
Macquet Quentin <qunetinmacquet@gmail.com>
2023-11-21 08:05:13 +0100

    :ADD Le début d'une grande histoire
    object -> jeu petits chevaux de base

---
+++ Petits_chevaux.py
@@ -0,0 +1,16 @@
+class Petit_chevaux(object):
+    def __init__(self) -> None:
+        self.vie = None
+        self.deplacer = None
+    
+    def seDeplacer():
+        pass
+
+    def est_en_vie():
+        pass
+
+    def sauter():# kill le joueur et le renvoie a la kasba
+        pass     
+
+    def casse_vide():
+        pass
