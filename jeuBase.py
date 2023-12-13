e09e295c80e58503b20a83f91a6c1af3a4012de0
hugoSauvage <126684588+hugoSauvage@users.noreply.github.com>
2023-12-13 09:19:33 +0100

    add: def ajouter_cheval , def sauter, des victoir, des sortir , def ajouter-plateau,
    construction du jeux en console

--- jeuBase.py
+++ jeuBase.py
@@ -5,16 +5,23 @@ class jeuBase:
     def __init__(self, joueurs) -> None:
         self.joueurs = [Horse(joueur) for joueur in joueurs]
         self.victoire = False
+        self.plateau = [None] * 56
+        self.position = [0]
+
    
-    def lancerDe(self):
+   
+    def lancerDe(self)-> int :
         return random.randint(1, 6)
     
-    def avancer_cheval(self, cheval, de):
+
+    def avancer_cheval(self, cheval, de)->int:
         cheval.position += de
     
-    def afficher_plateau(self):
-        pass
     
+    def afficher_plateau(self)->list:
+        return self.plateau
+    
+
     def jouer_tour(self, joueur):
         input(f"{joueur.nom} Appuyer sur Entrée pour lancer le dé")
         de = self.lancerDe()
@@ -22,10 +29,49 @@ class jeuBase:
         self.afficher_plateau()
         print(f"{joueur.nom} a avancé jusque la position {joueur.position}")
     
-    def ajouter_cheval(self, cheval):
-        self.chevaux.append(cheval)
 
-    def afficher_positions(self):
+    def ajouter_cheval(self, cheval)->list:
+        if self.couleur.cheval == "jaune":
+            self.plateau.append(cheval)
+            self.position = [0]
+        if self.couleur.cheval == "rouge":
+            self.plateau.append(cheval)
+            self.position = [15]
+        if self.couleur.cheval == "bleu":
+            self.plateau.append(cheval)
+            self.position = [29]
+        if self.couleur.cheval == "vert":
+            self.plateau.append(cheval)
+            self.position = [43]
+
+    def nbtour(self)-> bool:
+        return self.position == 2*self.position         
+    
+    def afficher_positions(self)-> str:
         for cheval in self.chevaux:
             print(f"{cheval.nom}: {cheval.position}")
+
+
+    def sauter( self, cheval_1,cheval_2):
+        if cheval_1.position == cheval_2.position:
+            cheval_1.position = cheval_2.position
+            self.plateau.pop(cheval_2)
+        if cheval_2.position == cheval_1.position:
+            cheval_2.position = cheval_1.position
+            self.plateau.pop(cheval_2)
+
+
+    def victoire( self,cheval)-> str:
+        while self.nbtour == False:
+            return self.afficher_positions
+        return ' vous avais gagner '
+
+
+    def sortir( self)->object:
+        if self.lancerDe == 6:
+            self.ajouter_cheval
+
     
+    
+
+
