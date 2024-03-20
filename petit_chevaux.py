e9795b60820fdd92141e54716ab2ddea52d38934
hugoSauvage <126684588+hugoSauvage@users.noreply.github.com>
2024-03-20 08:35:06 +0100

    Update petit_chevaux.py

--- petit_chevaux.py
+++ petit_chevaux.py
@@ -8,7 +8,7 @@ class Plateau:
     def ajouter_joueur(self, joueur_id, position_depart):
         self.joueurs[joueur_id] = {'position': position_depart, 'chevaux': [None, None]}
 
-    def deplacer_cheval(self, joueur_id, numero_cheval, deplacements):
+    def deplacer_cheval_plateau(self, joueur_id, numero_cheval, deplacements):
         position_cheval = self.joueurs[joueur_id]['chevaux'][numero_cheval]
         nouvelle_position = (position_cheval[0] + deplacements, position_cheval[1])
         if nouvelle_position[0] >= 0 and nouvelle_position[0] < 6 and nouvelle_position[1] >= 0 and nouvelle_position[1] < 12:
@@ -58,12 +58,43 @@ class Jeu:
 
         while True:
             if self.plateau.est_partie_terminee():
-                print(f"Le joueur {self.joueur_actuel_id} gagne!")
-                break
-
+                print(f"Le joueur {self.joueur_actuel_id} gagne!")   
             numero_cheval = None
             while numero_cheval is None:
-                print
+                print ("ce joueur n'exsite pas")
+
+
+class Joueur:
+    def __init__(self, identifiant):
+        self.identifiant = identifiant
+        self.score = 0
+
+class Partie:
+    def __init__(self, joueurs):
+        self.joueurs = joueurs
+        self.joueur_actuel = random.choice(joueurs)
+        self.lancer_de = 0
+        self.fin_de_partie = False
+
+    def lancer_le_de(self):
+        self.lancer_de = random.randint(1, 6)
+        print(f"{self.joueur_actuel.identifiant} a lancé un {self.lancer_de}")
+
+    def jouer_tour(self):
+        self.joueur_actuel.score += self.lancer_de
+        print(f"{self.joueur_actuel.identifiant} a maintenant un score de {self.joueur_actuel.score}")
+
+    def joueur_suivant(self):
+        self.joueur_actuel = self.joueurs[(self.joueurs.index(self.joueur_actuel) + 1) % len(self.joueurs)]
+
+    def boucle_de_jeu(self):
+        while not self.fin_de_partie:
+            self.lancer_le_de()
+            self.jouer_tour()
+            self.joueur_suivant()
+            if self.joueur_actuel.score >= 100:
+                self.fin_de_partie = True
+
 
 # Définir les positions de départ pour chaque joueur
 positions_depart = {
@@ -75,10 +106,13 @@ positions_depart = {
 
 # Créer une nouvelle instance de jeu
 jeu = Jeu()
+plateau = Plateau()
 
 # Ajouter des joueurs au jeu
 for joueur_id, position in positions_depart.items():
     jeu.ajouter_joueur(joueur_id, position)
 
 # Commencer le jeu
-jeu.jouer_partie(positions_depart)
+players = [Joueur(i) for i in range(1, 5)]
+game = Partie(players)
+game.boucle_de_jeu()
